964b9b5f82221a6ec2e94e9df571cf90a3fb61b2
linxiaopeng <359539865@qq.com>
2017-07-21 10:48:06 +0800

    no message

---
+++ FileManageTarget/FileManageTarget/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FileManageTarget
+//
+//  Created by pxl on 2017/7/20.
+//  Copyright © 2017年 pxl. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
